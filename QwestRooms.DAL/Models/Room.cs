deeabd9f66e7e7c784b3695e6791f8cb6e6d4ca1
Dmitriuses <shpacosky.dmutro.1999.mc@gmail.com>
2019-08-19 11:11:14 +0300

    asd

--- QwestRooms.DAL/Models/Room.cs
+++ QwestRooms.DAL/Models/Room.cs
@@ -8,6 +8,9 @@ namespace QwestRooms.DAL.Models
 {
     class Room
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
 
     }
 }
