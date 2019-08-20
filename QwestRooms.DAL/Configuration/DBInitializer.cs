bb7fcc1a726f829eb467c42db19d572d865a8d4d
Dmitriuses <shpacosky.dmutro.1999.mc@gmail.com>
2019-08-20 12:04:46 +0300

    add repoz and db inicializer

---
+++ QwestRooms.DAL/Configuration/DBInitializer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace QwestRooms.DAL.Configuration
+{
+    class DBinicialaiser : DropCreateDatabaseAlways<RoomsContext>
+    {
+        protected override void Seed(RoomsContext context)
+        {
+            
+        }
+    }
+}
